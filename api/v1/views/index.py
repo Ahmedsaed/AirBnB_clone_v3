df184f424e588c679415cb905c63eaf9f536c7e9
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-09-29 20:37:02 +0300

    API route for api status

---
+++ api/v1/views/index.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+returns json status response
+"""
+
+from api.v1.views import app_views
+from flask import jsonify, request
+
+@app_views.route('/status', methods=['GET'])
+def status():
+    """
+    function for status route that returns the status
+    """
+    if request.method == 'GET':
+        resp = {"status": "OK"}
+        return jsonify(resp)
