d40decd5ef10409e41dddb363116592db4ad9620
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-10-02 21:34:08 +0300

    hash passwords to md5

--- models/base_model.py
+++ models/base_model.py
@@ -65,10 +65,15 @@ class BaseModel:
             new_dict["created_at"] = new_dict["created_at"].strftime(time)
         if "updated_at" in new_dict:
             new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+        if 'reviews' in new_dict:
+            new_dict.pop('reviews', None)
+        if 'amenities' in new_dict:
+            new_dict.pop('amenities', None)
         if '_password' in new_dict:
             new_dict['password'] = new_dict['_password']
             new_dict.pop('_password', None)
         new_dict["__class__"] = self.__class__.__name__
+        new_dict.pop('_sa_instance_state', None)
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
         return new_dict
